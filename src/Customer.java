a78ff5c04de0b7e482039e7cc630a384a16a2a26
Jorge Iván Andrés Contreras Pereira <jorge@contreras.com.co>
2015-06-18 12:05:56 -0500

    Add customer

---
+++ src/Customer.java
@@ -0,0 +1,17 @@
+/**
+ * Created by jiacontrerasp on 6/18/15.
+ */
+public class Customer {
+    long creditCard;
+    private String[] interests;
+    String postalAddress;
+
+
+    public void addInterests(String[] interests) {
+        this.interests = interests;
+    }
+
+    public void removeInterests(String[] interests) {
+        this.interests = interests;
+    }
+}
