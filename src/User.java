76d226f27411571b19fe8e68063bad86c995754a
Jorge Iván Andrés Contreras Pereira <jorge@contreras.com.co>
2015-06-18 12:01:20 -0500

    Add User

---
+++ src/User.java
@@ -0,0 +1,14 @@
+/**
+ * Created by jiacontrerasp on 6/18/15.
+ */
+public class User {
+    String name;
+    String email;
+    String username;
+    String password;
+    String loginStatus;
+
+    public boolean verifyLogin(){
+        return true;
+    }
+}
